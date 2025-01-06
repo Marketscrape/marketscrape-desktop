774534ddedbb99c6de7017df5428ec6173809dc3
Bhavanvir Rai <bhavanvir.r@gmail.com>
2025-01-05 16:13:49 -0800

    Create minimal working example of fetching a listing by ID

--- marketscrape/frontend/src/components/HelloWorld.vue
+++ marketscrape/frontend/src/components/HelloWorld.vue
@@ -1,9 +1,23 @@
-<script lang="ts" setup>
-import { Button } from "./ui/button";
+<script setup lang="ts">
+import { ref } from "vue";
+import { Input } from "./ui/input/index.js";
+import { Button } from "./ui/button/index.js";
+
+import { GetMarketplaceListing } from "../../wailsjs/go/main/App.js";
+
+const id = ref("");
+const res = ref("");
+
+async function getMarketplaceListing() {
+  if (!id.value) {
+    return;
+  }
+  GetMarketplaceListing(id.value).then((resp) => (res.value = resp));
+}
 </script>
 
 <template>
-  <main>
-    <Button>Yo</Button>
-  </main>
+  <Input v-model="id" />
+  <Button @click="getMarketplaceListing">Submit</Button>
+  {{ res }}
 </template>
