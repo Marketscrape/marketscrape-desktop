3989f168b65f69a53fc2d9d3e9c70ac55f0949ef
Bhavanvir Rai <bhavanvir.r@gmail.com>
2025-01-08 21:34:24 -0800

    Update styling

--- marketscrape/frontend/src/components/HelloWorld.vue
+++ marketscrape/frontend/src/components/HelloWorld.vue
@@ -1,27 +1,94 @@
 <script setup lang="ts">
 import { ref } from "vue";
+import { useForm } from "vee-validate";
+import { toTypedSchema } from "@vee-validate/zod";
+import * as z from "zod";
+
 import { Input } from "./ui/input/index.js";
 import { Button } from "./ui/button/index.js";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card/index.js";
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./ui/form/index.js";
+
+import { Send } from "lucide-vue-next";
 
 import { GetMarketplaceListing } from "../../wailsjs/go/main/App.js";
 
-const id = ref("");
 const res = ref("");
 
-async function getMarketplaceListing() {
-  if (!id.value) {
-    return;
-  }
-  GetMarketplaceListing(id.value).then((resp) => {
+const formSchema = toTypedSchema(
+  z.object({
+    listingId: z
+      .string()
+      .length(15, { message: "Listing ID must be exactly 15 characters long" })
+      .regex(/^[0-9]{15}$/, {
+        message: "Listing ID must only contain 15 digits",
+      }),
+  }),
+);
+
+const form = useForm({
+  validationSchema: formSchema,
+});
+
+const onSubmit = form.handleSubmit(async (values) => {
+  const { listingId } = values;
+
+  GetMarketplaceListing(listingId).then((resp) => {
     res.value = JSON.stringify(resp, null, 2);
   });
-}
+});
 </script>
 
 <template>
-  <Input v-model="id" />
-  <Button @click="getMarketplaceListing">Submit</Button>
-  <pre>
-    {{ res }}
-  </pre>
+  <div class="flex h-screen items-center">
+    <form class="mx-auto" @submit="onSubmit">
+      <Card>
+        <CardHeader>
+          <CardTitle>Analyze a Marketplace Listing</CardTitle>
+          <CardDescription>
+            Enter the listing ID to retrieve detailed information and insights
+            about the product.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <FormField v-slot="{ componentField }" name="listingId">
+            <FormItem>
+              <FormLabel>Listing ID</FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  placeholder="Enter 15-digit Listing ID"
+                  v-bind="componentField"
+                />
+              </FormControl>
+              <FormDescription>
+                The unique 15-digit ID of the listing.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          </FormField>
+        </CardContent>
+        <CardFooter class="flex justify-end">
+          <Button type="submit">
+            <Send />
+            Submit
+          </Button>
+        </CardFooter>
+      </Card>
+    </form>
+  </div>
 </template>
