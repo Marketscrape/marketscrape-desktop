031f1dabf732da4f1a888485f4e1a15de99223bb
Alex Holland <alexholland@Alexs-iMac-2.lan>
2025-01-09 16:35:59 -0800

    Added url support

--- marketscrape/frontend/src/components/ListingInputCard.vue
+++ marketscrape/frontend/src/components/ListingInputCard.vue
@@ -28,18 +28,25 @@ import { Send, Loader2 } from "lucide-vue-next";
 
 import { GetMarketplaceListing } from "../../wailsjs/go/main/App.js";
 
+const extractListingIdFromUrl = (url: string) => {
+  const regex = /\/item\/([0-9]{15,16})/;
+  const match = url.match(regex);
+  return match ? match[1] : null;
+};
+
 const isLoading = ref(false);
 const emit = defineEmits(["listing"]);
-
 const formSchema = toTypedSchema(
   z.object({
     listingId: z
       .string()
-      .min(15, { message: "Listing ID must be at least 15 characters long" })
-      .max(16, { message: "Listing ID must be at most 16 characters long" })
-      .regex(/^[0-9]{15,16}$/, {
-        message: "Listing ID must only contain 15-16 digits",
-      }),
+      .refine(value => {
+        const isUrl = value.startsWith('http');
+        const isId = /^[0-9]{15,16}$/.test(value);
+        return isUrl || isId;
+      }, {
+        message: "Listing must be a valid URL or 15-16 digit ID",
+      })
   }),
 );
 
@@ -51,7 +58,13 @@ const onSubmit = form.handleSubmit(async (values) => {
   isLoading.value = true;
   const { listingId } = values;
 
-  GetMarketplaceListing(listingId).then((resp) => {
+  let idToUse = listingId;
+  if (listingId.startsWith("http")) {
+    // Extract the listing ID if URL is provided
+    idToUse = extractListingIdFromUrl(listingId) || listingId;
+  }
+
+  GetMarketplaceListing(idToUse).then((resp) => {
     emit("listing", JSON.stringify(resp, null, 2));
   });
 });
@@ -64,24 +77,21 @@ const onSubmit = form.handleSubmit(async (values) => {
         <CardHeader>
           <CardTitle>Analyze a Marketplace Listing</CardTitle>
           <CardDescription>
-            Enter the listing ID to retrieve detailed information and insights
+            Enter the listing URL or ID to retrieve detailed information and insights
             about the product.
           </CardDescription>
         </CardHeader>
         <CardContent>
           <FormField v-slot="{ componentField }" name="listingId">
             <FormItem v-auto-animate>
-              <FormLabel>Listing ID</FormLabel>
+              <FormLabel>Listing ID or URL</FormLabel>
               <FormControl>
                 <Input
                   type="text"
-                  placeholder="Enter 15- or 16-digit Listing ID"
+                  placeholder="Enter Marketplace Listing URL or ID"
                   v-bind="componentField"
                 />
               </FormControl>
-              <FormDescription>
-                The unique 15- or 16-digit ID of the listing.
-              </FormDescription>
               <FormMessage />
             </FormItem>
           </FormField>
