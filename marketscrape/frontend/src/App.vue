210bd8a11fee7bde3568b779538d9469d4cfeec9
Bhavanvir Rai <bhavanvir.r@gmail.com>
2025-01-20 21:39:44 -0800

    Add in button for triggering Ollama chat

--- marketscrape/frontend/src/App.vue
+++ marketscrape/frontend/src/App.vue
@@ -8,7 +8,7 @@ import { ArrowLeft } from "lucide-vue-next";
 import ListingInputCard from "./components/ListingInputCard.vue";
 import ProductListingCard from "./components/ProductListingCard.vue";
 import VehicleListingCard from "./components/VehicleListingCard.vue";
-
+import OllamaChat from "./components/OllamaChat.vue";
 import { main } from "./../wailsjs/go/models";
 
 enum Categories {
@@ -38,26 +38,32 @@ const resetListing = () => {
 </script>
 
 <template>
-  <div>
-    <ListingInputCard v-if="!listing" @listing="handleListing" />
-    <div v-else class="space-y-8">
-      <div v-if="error">
-        {{ error }}
+  <div class="h-[calc(100vh-4rem)] relative">
+    <div class="absolute inset-0 grid place-content-center">
+      <ListingInputCard v-if="!listing" @listing="handleListing" />
+      <div v-else class="space-y-8">
+        <div v-if="error">
+          {{ error }}
+        </div>
+        <div v-else class="grid grid-flow-row gap-8">
+          <Button @click="resetListing" class="w-fit">
+            <ArrowLeft />
+            Go Back
+          </Button>
+          <ProductListingCard
+            v-if="listingType === Categories.Product"
+            :listing="listing"
+          />
+          <VehicleListingCard
+            v-if="listingType === Categories.Vehicle"
+            :listing="listing"
+          />
+        </div>
       </div>
-      <div v-else class="grid grid-flow-row gap-8">
-        <Button @click="resetListing" class="w-fit">
-          <ArrowLeft />
-          Go Back
-        </Button>
-        <ProductListingCard
-          v-if="listingType === Categories.Product"
-          :listing="listing"
-        />
-        <VehicleListingCard
-          v-if="listingType === Categories.Vehicle"
-          :listing="listing"
-        />
-      </div>
+    </div>
+
+    <div class="absolute bottom-0 right-0">
+      <OllamaChat />
     </div>
   </div>
 </template>
